9a289f4fe94ad5ba20112414508ecf55cc0db454
Alisa Semenova <salisa4227@gmail.com>
2022-10-31 21:26:45 +0300

    Доп.задание1

---
+++ src/main/java/org/example/Primitives.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Primitives {
+    byte b = 3;
+    short s = 17;
+    int i = 56;
+    long l = 258;
+    char ch = 'v';
+    float f = 1.2F;
+    double e = 25.3;
+    boolean boo = true;
+}
+
---
+++ src/main/java/org/example/Wrapper.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Wrapper {
+    Byte b = 12;
+    Short s = 3;
+    Integer i = 121;
+    Double d = 45d;
+    Long l = 729L;
+    Character c = 'm';
+    Float f = 12.25f;
+    Boolean boo = true;
+}
